60aeaa9994ca3777bccd33d29416855a5b09bc8d
R0st <dotarost@mail.ru>
2021-10-20 20:39:22 +0300

    add controller middlewares

--- app.js
+++ app.js
@@ -1,30 +1,31 @@
-const express = require('express')
-const logger = require('morgan')
-const cors = require('cors')
+const express = require("express");
+const logger = require("morgan");
+const cors = require("cors");
 
-const contactsRouter = require('./routes/api/contacts')
+const contactsRouter = require("./routes/api/contacts");
 
-const app = express()
+const app = express();
 
-const formatsLogger = app.get('env') === 'development' ? 'dev' : 'short'
+const formatsLogger = app.get("env") === "development" ? "dev" : "short";
 
-app.use(logger(formatsLogger))
-app.use(cors())
-app.use(express.json())
+app.use(logger(formatsLogger));
+app.use(cors());
+app.use(express.json());
 
-app.use('/api/contacts', contactsRouter)
-
-app.use((req, res) => {
-  res.status(404).json({ message: 'Not found' })
-})
+app.use("/api/contacts", contactsRouter);
 
 app.use((req, res) => {
-  res.status(400).json({ message: "missing required name field" })
-})
+  res.status(404).json({ message: "Not found" });
+});
 
-app.use((err, req, res, next) => {    //универсальный обработчик для ошибки, всегда с 4 аргументами
+// app.use((req, res) => {
+//   res.status(400).json({ message: "missing required name field" })
+// })
+
+app.use((err, req, res, next) => {
+  //универсальный обработчик для ошибки, всегда с 4 аргументами
   const { status = 500, message = "Server error" } = err;
-  res.status(status).json({ message })
-})
+  res.status(status).json({ message });
+});
 
-module.exports = app
+module.exports = app;
--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -1,147 +1,25 @@
-// const express = require('express')
-// const router = express.Router()
-
-// router.get('/', async (req, res, next) => {
-//   res.json({ message: 'home work done!' })
-// })
-
-// router.get('/:contactId', async (req, res, next) => {
-//   res.json({ message: 'template message' })
-// })
-
-// router.post('/', async (req, res, next) => {
-//   res.json({ message: 'template message' })
-// })
-
-// router.delete('/:contactId', async (req, res, next) => {
-//   res.json({ message: 'template message' })
-// })
-
-// router.patch('/:contactId', async (req, res, next) => {
-//   res.json({ message: 'template message' })
-// })
-
-// module.exports = router
-
-
 const express = require("express");
-const { NotFound, BadRequest } = require("http-errors");
 
-// const createError = require("http-errors");
-const Joi = require("joi");
+const { controllerWrapper, validation } = require("../../middlewares");
 
-const contactsOperations = require("../../model/contacts");
+const { contactSchema } = require("../../schemas");
 
-const joiShema = Joi.object({
-    name: Joi.string().required(),
-    email: Joi.string().required(),
-    phone: Joi.number().required()
-})
+const { contacts: ctrl } = require("../../controllers");
 
 const router = express.Router();
 
-router.get("/", async(req, res, next) => {
-    try {
-        const contacts = await contactsOperations.listContacts();
-        // throw new Error("не удалось прочитать файл");
-        res.json(contacts);
-    }
-    catch (error) {
-        next(error);
-        // res.status(500).json({
-        //     status: "error",
-        //     code: 500,
-        //     message: error.message
-        // })
-    }
-});
+router.get("/", controllerWrapper(ctrl.getAll));
 
-router.get("/:contactId", async (req, res, next) => {
-    try {
-        const { contactId } = req.params;
-        const contact = await contactsOperations.getContactById(Number(contactId));
-        if (!contact) {
-            throw new NotFound("Not Found");
-            // throw new createError(404, `Contact with id=${id} not found`);
+router.get("/:contactId", controllerWrapper(ctrl.getById));
 
-            // const error = new Error(`Contact with id=${id} not found`);
-            // error.status = 404;
-            // throw error;
+router.post("/", validation(contactSchema), controllerWrapper(ctrl.add));
 
-            // res.status(404).json({
-            //     status: "error",
-            //     code: 404,
-            //     message: `Contact with id=${id} not found`
-            // });
-            // return
-        }
-        res.json(contact);
-    }
-    catch (error) {
-        next(error)
-    }
-});
+router.put(
+  "/:contactId",
+  validation(contactSchema),
+  controllerWrapper(ctrl.updateById)
+);
 
-router.post("/", async(req, res, next) => {
-    try {
-        const { error } = joiShema.validate(req.body);
-        if (error) {
-            throw new BadRequest(error.message);
-        }
-        const result = await contactsOperations.addContact(req.body);
-        res.status(201).json({
-            status: "success",
-            code: 201,
-            data: {
-                result
-            }
-        })
-    }
-    catch (error) {
-        next(error);
-    }
-})
-
-router.put("/:contactId", async (req, res, next) => {
-    try {
-        const { error } = joiShema.validate(req.body);
-        if (error) {
-            throw new BadRequest({"message": "missing fields"});
-        }
-        const { contactId } = req.params;
-        const result = await contactsOperations.updateById(contactId, req.body);
-        if (!result) {
-            throw new NotFound("Not found");
-        }
-        res.json({
-            status: "success",
-            code: 200,
-            data: {
-                result
-            }
-        })
-    }
-    catch (error) {
-        next(error);
-    }
-})
-
-router.delete("/:contactId", async (req, res, next) => {
-    try {
-        const { contactId } = req.params;
-        const result = await contactsOperations.removeContact(Number(contactId));
-        if (!result) {
-            throw new NotFound("Not Found");
-        }
-        res.json({
-            status: "success",
-            code: 200,
-            message: "contact deleted"
-        })
-    }
-    catch (error) {
-        next(error)
-    }
-})
+router.delete("/:contactId", controllerWrapper(ctrl.removeById));
 
 module.exports = router;
