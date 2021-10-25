1db76a805a82df0c854f3bce8006308e7cc9c95d
R0st <dotarost@mail.ru>
2021-10-25 12:37:55 +0300

    add .env

--- mongodb-project/app.js
+++ mongodb-project/app.js
@@ -1,7 +1,10 @@
 const mongoose = require("mongoose");
+// const dotenv = require("dotenv");
+// dotenv.config();
+require("dotenv").config();
 
-const { DB_HOST } = require("./config");
-
+const { DB_HOST } = process.env;
+// console.log(DB_HOST);
 mongoose
   .connect(DB_HOST, {
     useNewUrlParser: true,
