6cfa21613d6532c2cf43d4a2650842de6109b444
R0st <dotarost@mail.ru>
2021-11-01 03:40:13 +0200

    add pagination

--- controllers/contacts/getAll.js
+++ controllers/contacts/getAll.js
@@ -1,7 +1,16 @@
 const { Contact } = require("../../models");
 
 const getAll = async (req, res) => {
-  const result = await Contact.find({}, "_id name email phone favorite"); //перечисляем в "" что нужно чтобы пришло без лишних полей
+  // console.log(req.query);
+  const { page = 1, limit = 20 } = req.query;
+  const skip = (page - 1) * limit;
+  const { _id } = req.user;
+
+  const result = await Contact.find({ owner: _id }, "_id content owner", {
+    skip,
+    limit: +limit,
+  }).populate("owner", "email"); //перечисляем в "" что нужно чтобы пришло без лишних полей
+
   res.json({
     status: "success",
     code: 200,
